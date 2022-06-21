61f9768efcc15311a162ff795de2761bb686438f
Sumit Sheoran <sumitsheoran521@gmail.com>
2022-06-21 19:00:28 +0530

    added multiply

---
+++ multiply.java
@@ -0,0 +1,9 @@
+package NewtonSchool;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 67;
+        System.out.println(a * b);
+    }
+}
